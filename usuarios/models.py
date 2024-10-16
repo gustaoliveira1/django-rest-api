563c8c90ccff28e1731805fab17ceb13a808aa22
Gustavo Oliveira <gusta01.oliveira@gmail.com>
2024-10-16 16:35:59 -0300

    feat: add user authentication route

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 
 
 class Usuario(AbstractBaseUser):
     nome = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     data_de_nascimento = models.DateField()
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,8 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 from usuarios.views import UsuarioRegisterView
 
 urlpatterns = [
     path('', UsuarioRegisterView.as_view(), name="user_register"),
+    path('auth/', TokenObtainPairView.as_view(), name="token_obtain_pair'"),
 ]
