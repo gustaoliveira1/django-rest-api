3518d012d0e331e25e763338012fdd482ba01a8c
Gustavo Oliveira <gusta01.oliveira@gmail.com>
2024-10-16 16:56:27 -0300

    feat: add routes for updating, deleting and reading users

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,9 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
-from usuarios.views import UsuarioRegisterView
+from usuarios.views import UsuarioRegisterView, UsuarioDetailsView
 
 urlpatterns = [
     path('', UsuarioRegisterView.as_view(), name="user_register"),
+    path('<int:pk>/', UsuarioDetailsView.as_view(), name="user_details"),
     path('auth/', TokenObtainPairView.as_view(), name="token_obtain_pair'"),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,16 @@
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from usuarios.models import Usuario
 from usuarios.serializers import UsuarioSerializer
 
 
 class UsuarioRegisterView(CreateAPIView):
-    queryset = Usuario
+    queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
     permission_classes = [AllowAny]
+
+
+class UsuarioDetailsView(RetrieveUpdateDestroyAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    permission_classes = [IsAuthenticated]
